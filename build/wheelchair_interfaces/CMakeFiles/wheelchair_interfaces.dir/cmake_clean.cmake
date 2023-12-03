0f910b716de432835080a49b127191b02d70726d
Bintang Dwi Marthen <89301265+Marthenn@users.noreply.github.com>
2023-12-03 20:28:37 +0700

    Delete build directory

--- build/wheelchair_interfaces/CMakeFiles/wheelchair_interfaces.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/wheelchair_interfaces"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/wheelchair_interfaces.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/wheelchair_interfaces/rosidl_cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(wheelchair_interfaces_IDL_FILES "action/Move.idl")
-set(wheelchair_interfaces_INTERFACE_FILES "action/Move.action")
